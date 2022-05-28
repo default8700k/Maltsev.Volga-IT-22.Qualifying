980bb2553f572d6d9896de2ddef532fc0622628c
Victor Maltsev <default8700k@gmail.com>
2022-05-28 19:25:59 +0300

    Add VersionController & fix namespace.

---
+++ src/Maltsev.Volga-IT'22.Qualifying.WebApplication/Controllers/VersionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Maltsev.Volga_IT_22.Qualifying.WebApplication;
+
+[Route("[controller]")]
+public class VersionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetVersion() =>
+        new OkObjectResult(
+            new
+            {
+                Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString()
+            }
+        );
+}
